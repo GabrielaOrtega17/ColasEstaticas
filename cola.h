a320b75d955f1a237aeda4a9b8fe3157b3709c5b
GabrielaOrtega17 <142334310+GabrielaOrtega17@users.noreply.github.com>
2023-09-26 12:18:36 -0600

    Add files via upload

---
+++ cola.h
@@ -0,0 +1,380 @@
+%PDF-1.3
+3 0 obj
+<</Type /Page
+/Parent 1 0 R
+/Resources 2 0 R
+/Annots []
+/Contents 4 0 R>>
+endobj
+4 0 obj
+<</Length 16046>>
+stream
+q
+1.000 0.000 0.000 -1.000 0.000 841.890 cm
+2 J
+0.57 w
+BT /F1 10.00 Tf ET
+0 G
+0 g
+q 0.000 0.627 0.000 rg BT 1 0 0 -1 28.35 37.02 Tm 0 Tr (#ifndef COLA_H_INCLUDED) Tj  ET Q
+q 0.000 0.627 0.000 rg BT 1 0 0 -1 28.35 48.35 Tm 0 Tr (#define COLA_H_INCLUDED) Tj  ET Q
+q 0.000 0.627 0.000 rg BT 1 0 0 -1 28.35 71.03 Tm 0 Tr (#include <iostream>) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 28.35 93.71 Tm 0 Tr (using namespace ) Tj  ET Q
+q 0.000 0.627 0.000 rg BT 1 0 0 -1 124.35 93.71 Tm 0 Tr (std) Tj  ET Q
+BT /F1 10.00 Tf ET
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 142.35 93.71 Tm 0 Tr (;) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 28.35 116.39 Tm 0 Tr (const int ) Tj  ET Q
+BT /F1 10.00 Tf ET
+1.000 1.000 1.000 rg
+88.35 107.72 18.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 88.35 116.39 Tm 0 Tr (TAM) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 106.35 116.39 Tm 0 Tr (=) Tj  ET Q
+q 0.941 0.000 0.941 rg BT 1 0 0 -1 112.35 116.39 Tm 0 Tr (100) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 130.35 116.39 Tm 0 Tr (;) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 28.35 139.06 Tm 0 Tr (class ) Tj  ET Q
+BT /F1 10.00 Tf ET
+1.000 1.000 1.000 rg
+64.35 130.39 66.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 64.35 139.06 Tm 0 Tr (Estudiante ) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 130.35 139.06 Tm 0 Tr ({) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 28.35 150.40 Tm 0 Tr (private) Tj  ET Q
+BT /F1 10.00 Tf ET
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 70.35 150.40 Tm 0 Tr (:) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 28.35 161.74 Tm 0 Tr (    ) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.627 0.000 rg BT 1 0 0 -1 52.35 161.74 Tm 0 Tr (std) Tj  ET Q
+BT /F1 10.00 Tf ET
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 70.35 161.74 Tm 0 Tr (::) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.627 0.000 rg BT 1 0 0 -1 82.35 161.74 Tm 0 Tr (string ) Tj  ET Q
+BT /F1 10.00 Tf ET
+1.000 1.000 1.000 rg
+124.35 153.07 36.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 124.35 161.74 Tm 0 Tr (nombre) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 160.35 161.74 Tm 0 Tr (;) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 28.35 173.08 Tm 0 Tr (    ) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 52.35 173.08 Tm 0 Tr (int ) Tj  ET Q
+BT /F1 10.00 Tf ET
+1.000 1.000 1.000 rg
+76.35 164.41 24.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 76.35 173.08 Tm 0 Tr (edad) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 100.35 173.08 Tm 0 Tr (;) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 28.35 184.42 Tm 0 Tr (public) Tj  ET Q
+BT /F1 10.00 Tf ET
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 64.35 184.42 Tm 0 Tr (:) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 28.35 195.76 Tm 0 Tr (    ) Tj  ET Q
+1.000 1.000 1.000 rg
+52.35 187.09 60.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 52.35 195.76 Tm 0 Tr (Estudiante) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 112.35 195.76 Tm 0 Tr (\(\);) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 28.35 207.09 Tm 0 Tr (    ) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 52.35 207.09 Tm 0 Tr (void operator) Tj  ET Q
+BT /F1 10.00 Tf ET
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 130.35 207.09 Tm 0 Tr (=\() Tj  ET Q
+1.000 1.000 1.000 rg
+142.35 198.43 60.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 142.35 207.09 Tm 0 Tr (Estudiante) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 202.35 207.09 Tm 0 Tr (& ) Tj  ET Q
+1.000 1.000 1.000 rg
+214.35 198.43 6.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 214.35 207.09 Tm 0 Tr (x) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 220.35 207.09 Tm 0 Tr (\);) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 28.35 218.43 Tm 0 Tr (    ) Tj  ET Q
+1.000 1.000 1.000 rg
+52.35 209.76 60.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 52.35 218.43 Tm 0 Tr (Estudiante) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 112.35 218.43 Tm 0 Tr (\() Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.627 0.000 rg BT 1 0 0 -1 118.35 218.43 Tm 0 Tr (std) Tj  ET Q
+BT /F1 10.00 Tf ET
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 136.35 218.43 Tm 0 Tr (::) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.627 0.000 rg BT 1 0 0 -1 148.35 218.43 Tm 0 Tr (string ) Tj  ET Q
+BT /F1 10.00 Tf ET
+1.000 1.000 1.000 rg
+190.35 209.76 6.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 190.35 218.43 Tm 0 Tr (n) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 196.35 218.43 Tm 0 Tr (, ) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 208.35 218.43 Tm 0 Tr (int ) Tj  ET Q
+BT /F1 10.00 Tf ET
+1.000 1.000 1.000 rg
+232.35 209.76 6.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 232.35 218.43 Tm 0 Tr (e) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 238.35 218.43 Tm 0 Tr (\);) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 28.35 229.77 Tm 0 Tr (    ) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 52.35 229.77 Tm 0 Tr (friend ) Tj  ET Q
+q 0.000 0.627 0.000 rg BT 1 0 0 -1 94.35 229.77 Tm 0 Tr (std) Tj  ET Q
+BT /F1 10.00 Tf ET
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 112.35 229.77 Tm 0 Tr (::) Tj  ET Q
+1.000 1.000 1.000 rg
+124.35 221.10 42.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 124.35 229.77 Tm 0 Tr (ostream) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 166.35 229.77 Tm 0 Tr (& ) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 178.35 229.77 Tm 0 Tr (operator) Tj  ET Q
+BT /F1 10.00 Tf ET
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 226.35 229.77 Tm 0 Tr (<<\( ) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.627 0.000 rg BT 1 0 0 -1 250.35 229.77 Tm 0 Tr (std) Tj  ET Q
+BT /F1 10.00 Tf ET
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 268.35 229.77 Tm 0 Tr (::) Tj  ET Q
+1.000 1.000 1.000 rg
+280.35 221.10 42.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 280.35 229.77 Tm 0 Tr (ostream) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 322.35 229.77 Tm 0 Tr (&, ) Tj  ET Q
+1.000 1.000 1.000 rg
+340.35 221.10 60.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 340.35 229.77 Tm 0 Tr (Estudiante) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 400.35 229.77 Tm 0 Tr (&\);) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 28.35 241.11 Tm 0 Tr (    ) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 52.35 241.11 Tm 0 Tr (friend ) Tj  ET Q
+q 0.000 0.627 0.000 rg BT 1 0 0 -1 94.35 241.11 Tm 0 Tr (std) Tj  ET Q
+BT /F1 10.00 Tf ET
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 112.35 241.11 Tm 0 Tr (::) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.627 0.000 rg BT 1 0 0 -1 124.35 241.11 Tm 0 Tr (istream) Tj  ET Q
+BT /F1 10.00 Tf ET
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 166.35 241.11 Tm 0 Tr (& ) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 178.35 241.11 Tm 0 Tr (operator) Tj  ET Q
+BT /F1 10.00 Tf ET
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 226.35 241.11 Tm 0 Tr (>>\( ) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.627 0.000 rg BT 1 0 0 -1 250.35 241.11 Tm 0 Tr (std) Tj  ET Q
+BT /F1 10.00 Tf ET
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 268.35 241.11 Tm 0 Tr (::) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.627 0.000 rg BT 1 0 0 -1 280.35 241.11 Tm 0 Tr (istream) Tj  ET Q
+BT /F1 10.00 Tf ET
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 322.35 241.11 Tm 0 Tr (&, ) Tj  ET Q
+1.000 1.000 1.000 rg
+340.35 232.44 60.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 340.35 241.11 Tm 0 Tr (Estudiante) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 400.35 241.11 Tm 0 Tr (&\);) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 28.35 252.45 Tm 0 Tr (};) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 28.35 275.13 Tm 0 Tr (class ) Tj  ET Q
+BT /F1 10.00 Tf ET
+1.000 1.000 1.000 rg
+64.35 266.46 30.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 64.35 275.13 Tm 0 Tr (Cola ) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 94.35 275.13 Tm 0 Tr ({) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 28.35 286.46 Tm 0 Tr (private) Tj  ET Q
+BT /F1 10.00 Tf ET
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 70.35 286.46 Tm 0 Tr (:) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 28.35 297.80 Tm 0 Tr (    ) Tj  ET Q
+1.000 1.000 1.000 rg
+52.35 289.13 96.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 52.35 297.80 Tm 0 Tr (Estudiante datos) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 148.35 297.80 Tm 0 Tr ([) Tj  ET Q
+1.000 1.000 1.000 rg
+154.35 289.13 18.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 154.35 297.80 Tm 0 Tr (TAM) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 172.35 297.80 Tm 0 Tr (];) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 28.35 309.14 Tm 0 Tr (    ) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 52.35 309.14 Tm 0 Tr (int ) Tj  ET Q
+BT /F1 10.00 Tf ET
+1.000 1.000 1.000 rg
+76.35 300.47 18.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 76.35 309.14 Tm 0 Tr (ult) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 94.35 309.14 Tm 0 Tr (;) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 28.35 320.48 Tm 0 Tr (    ) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 52.35 320.48 Tm 0 Tr (bool ) Tj  ET Q
+BT /F1 10.00 Tf ET
+1.000 1.000 1.000 rg
+82.35 311.81 42.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 82.35 320.48 Tm 0 Tr (elimina) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 124.35 320.48 Tm 0 Tr (\() Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 130.35 320.48 Tm 0 Tr (int ) Tj  ET Q
+BT /F1 10.00 Tf ET
+1.000 1.000 1.000 rg
+154.35 311.81 18.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 154.35 320.48 Tm 0 Tr (pos) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 172.35 320.48 Tm 0 Tr (\);) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 28.35 331.82 Tm 0 Tr (    ) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 52.35 331.82 Tm 0 Tr (int ) Tj  ET Q
+BT /F1 10.00 Tf ET
+1.000 1.000 1.000 rg
+76.35 323.15 42.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 76.35 331.82 Tm 0 Tr (inserta) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 118.35 331.82 Tm 0 Tr (\() Tj  ET Q
+1.000 1.000 1.000 rg
+124.35 323.15 60.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 124.35 331.82 Tm 0 Tr (Estudiante) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 184.35 331.82 Tm 0 Tr (& ) Tj  ET Q
+1.000 1.000 1.000 rg
+196.35 323.15 24.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 196.35 331.82 Tm 0 Tr (elem) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 220.35 331.82 Tm 0 Tr (, ) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 232.35 331.82 Tm 0 Tr (int ) Tj  ET Q
+BT /F1 10.00 Tf ET
+1.000 1.000 1.000 rg
+256.35 323.15 18.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 256.35 331.82 Tm 0 Tr (pos) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 274.35 331.82 Tm 0 Tr (\);) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 28.35 343.16 Tm 0 Tr (public) Tj  ET Q
+BT /F1 10.00 Tf ET
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 64.35 343.16 Tm 0 Tr (:) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 28.35 354.50 Tm 0 Tr (    ) Tj  ET Q
+1.000 1.000 1.000 rg
+52.35 345.83 24.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 52.35 354.50 Tm 0 Tr (Cola) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 76.35 354.50 Tm 0 Tr (\(\):) Tj  ET Q
+1.000 1.000 1.000 rg
+94.35 345.83 18.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 94.35 354.50 Tm 0 Tr (ult) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 112.35 354.50 Tm 0 Tr (\(-) Tj  ET Q
+q 0.941 0.000 0.941 rg BT 1 0 0 -1 124.35 354.50 Tm 0 Tr (1) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 130.35 354.50 Tm 0 Tr (\) {}) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 28.35 365.83 Tm 0 Tr (    ) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 52.35 365.83 Tm 0 Tr (bool ) Tj  ET Q
+BT /F1 10.00 Tf ET
+1.000 1.000 1.000 rg
+82.35 357.17 30.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 82.35 365.83 Tm 0 Tr (vacia) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 112.35 365.83 Tm 0 Tr (\(\)) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 124.35 365.83 Tm 0 Tr (const) Tj  ET Q
+BT /F1 10.00 Tf ET
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 154.35 365.83 Tm 0 Tr (;) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 28.35 377.17 Tm 0 Tr (    ) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 52.35 377.17 Tm 0 Tr (bool ) Tj  ET Q
+BT /F1 10.00 Tf ET
+1.000 1.000 1.000 rg
+82.35 368.50 30.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 82.35 377.17 Tm 0 Tr (llena) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 112.35 377.17 Tm 0 Tr (\(\)) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 124.35 377.17 Tm 0 Tr (const) Tj  ET Q
+BT /F1 10.00 Tf ET
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 154.35 377.17 Tm 0 Tr (;) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 28.35 388.51 Tm 0 Tr (    ) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 52.35 388.51 Tm 0 Tr (int ) Tj  ET Q
+BT /F1 10.00 Tf ET
+1.000 1.000 1.000 rg
+76.35 379.84 36.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 76.35 388.51 Tm 0 Tr (ultimo) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 112.35 388.51 Tm 0 Tr (\(\)) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 124.35 388.51 Tm 0 Tr (const) Tj  ET Q
+BT /F1 10.00 Tf ET
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 154.35 388.51 Tm 0 Tr (;) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 28.35 399.85 Tm 0 Tr (    ) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 52.35 399.85 Tm 0 Tr (friend ) Tj  ET Q
+q 0.000 0.627 0.000 rg BT 1 0 0 -1 94.35 399.85 Tm 0 Tr (std) Tj  ET Q
+BT /F1 10.00 Tf ET
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 112.35 399.85 Tm 0 Tr (::) Tj  ET Q
+1.000 1.000 1.000 rg
+124.35 391.18 42.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 124.35 399.85 Tm 0 Tr (ostream) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 166.35 399.85 Tm 0 Tr (& ) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 178.35 399.85 Tm 0 Tr (operator) Tj  ET Q
+BT /F1 10.00 Tf ET
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 226.35 399.85 Tm 0 Tr (<<\() Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.627 0.000 rg BT 1 0 0 -1 244.35 399.85 Tm 0 Tr (std) Tj  ET Q
+BT /F1 10.00 Tf ET
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 262.35 399.85 Tm 0 Tr (::) Tj  ET Q
+1.000 1.000 1.000 rg
+274.35 391.18 48.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 274.35 399.85 Tm 0 Tr (ostream ) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 322.35 399.85 Tm 0 Tr (& ) Tj  ET Q
+1.000 1.000 1.000 rg
+334.35 391.18 6.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 334.35 399.85 Tm 0 Tr (o) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 340.35 399.85 Tm 0 Tr (, ) Tj  ET Q
+1.000 1.000 1.000 rg
+352.35 391.18 24.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 352.35 399.85 Tm 0 Tr (Cola) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 376.35 399.85 Tm 0 Tr (& ) Tj  ET Q
+1.000 1.000 1.000 rg
+388.35 391.18 6.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 388.35 399.85 Tm 0 Tr (L) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 394.35 399.85 Tm 0 Tr (\);) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 28.35 411.19 Tm 0 Tr (    ) Tj  ET Q
+BT /F2 10.00 Tf ET
+q 0.000 0.000 0.627 rg BT 1 0 0 -1 52.35 411.19 Tm 0 Tr (void ) Tj  ET Q
+BT /F1 10.00 Tf ET
+1.000 1.000 1.000 rg
+82.35 402.52 42.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 82.35 411.19 Tm 0 Tr (enqueue) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 124.35 411.19 Tm 0 Tr (\() Tj  ET Q
+1.000 1.000 1.000 rg
+130.35 402.52 60.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 130.35 411.19 Tm 0 Tr (Estudiante) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 190.35 411.19 Tm 0 Tr (& ) Tj  ET Q
+1.000 1.000 1.000 rg
+202.35 402.52 24.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 202.35 411.19 Tm 0 Tr (elem) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 226.35 411.19 Tm 0 Tr (\);) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 28.35 422.53 Tm 0 Tr (    ) Tj  ET Q
+1.000 1.000 1.000 rg
+52.35 413.86 60.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 52.35 422.53 Tm 0 Tr (Estudiante) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 112.35 422.53 Tm 0 Tr (& ) Tj  ET Q
+1.000 1.000 1.000 rg
+124.35 413.86 42.00 11.34 re f q 0.000 0.000 0.000 rg BT 1 0 0 -1 124.35 422.53 Tm 0 Tr (dequeue) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 166.35 422.53 Tm 0 Tr (\(\);) Tj  ET Q
+q 1.000 0.000 0.000 rg BT 1 0 0 -1 28.35 445.20 Tm 0 Tr (};) Tj  ET Q
+q 0.000 0.627 0.000 rg BT 1 0 0 -1 28.35 501.90 Tm 0 Tr (#endif ) Tj  ET Q
+q 0.596 0.596 0.851 rg BT 1 0 0 -1 70.35 501.90 Tm 0 Tr (// COLA_H_INCLUDED) Tj  ET Q
+Q
+
+endstream
+endobj
+1 0 obj
+<</Type /Pages
+/Kids [3 0 R ]
+/Count 1
+/MediaBox [0 0 595.276 841.890]
+>>
+endobj
+5 0 obj
+<</Type /Font
+/BaseFont /Courier-Bold
+/Subtype /Type1
+/Encoding /WinAnsiEncoding
+>>
+endobj
+6 0 obj
+<</Type /Font
+/BaseFont /Courier
+/Subtype /Type1
+/Encoding /WinAnsiEncoding
+>>
+endobj
+2 0 obj
+<<
+/ProcSet [/PDF /Text /ImageB /ImageC /ImageI]
+/Font <<
+/F2 5 0 R
+/F1 6 0 R
+>>
+/XObject <<
+>>
+>>
+endobj
+7 0 obj
+<<
+/Names []
+>>
+endobj
+8 0 obj
+<<
+/Producer (þÿ w x P d f D o c u m e n t   0 . 9 . 8)
+/CreationDate (D:20230926121759)
+>>
+endobj
+9 0 obj
+<<
+/Type /Catalog
+/Pages 1 0 R
+/OpenAction [3 0 R /FitH null]
+/PageLayout /OneColumn
+>>
+endobj
+xref
+0 10
+0000000000 65535 f 
+0000016195 00000 n 
+0000016477 00000 n 
+0000000009 00000 n 
+0000000098 00000 n 
+0000016284 00000 n 
+0000016383 00000 n 
+0000016591 00000 n 
+0000016622 00000 n 
+0000016729 00000 n 
+trailer
+<<
+/Size 10
+/Root 9 0 R
+/Info 8 0 R
+>>
+startxref
+16832
+%%EOF
